2f391fffa5225c30b4342d98d8b0b0de8a56db96
Karen Vaganyan <k.vaganyan@gmail.com>
2020-01-11 22:27:01 -0700

    Commiting a version of directions reduction. Passes the basic sample tests but not the more complex ones

---
+++ directions-reduction/directions-reduction/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Creating a few different test cases to do more thorough testing
+        Random randomizer = new Random();
+        string[] dirArray = new string[] { "NORTH", "SOUTH", "WEST", "EAST" };
+
+        for (int i = 0; i <= 10; i++)
+        {
+            string[] dirList = new string[randomizer.Next(1, 10)];
+
+            for (int j = 0; j <= dirList.Count()-1; j++)
+            {
+                dirList[j] = dirArray[randomizer.Next(0, 3)];
+            }
+
+            Console.WriteLine(string.Join(",", dirList));
+        }
+
+        Console.ReadLine();
+    }
+}
+
+public class DirReduction
+{
+
+    public static string[] dirReduc(String[] arr)
+    {
+        List<string> dirInstructions = new List<string>();
+        dirInstructions = arr.Select(a => a.ToUpper()).ToList();
+
+        for (int i = dirInstructions.Count - 1; i > 0; i--)
+        {
+            switch (arr[i])
+            {
+                case "NORTH":
+                    if (arr[i - 1] == "SOUTH")
+                    {
+                        dirInstructions.RemoveAt(i);
+                        dirInstructions.RemoveAt(i - 1);
+                        i = dirInstructions.Count - 1;
+                    }
+                    break;
+                case "SOUTH":
+                    if (arr[i - 1] == "NORTH")
+                    {
+                        dirInstructions.RemoveAt(i);
+                        dirInstructions.RemoveAt(i - 1);
+                        i = dirInstructions.Count - 1;
+                    }
+                    break;
+                case "EAST":
+                    if (arr[i - 1] == "WEST")
+                    {
+                        dirInstructions.RemoveAt(i);
+                        dirInstructions.RemoveAt(i - 1);
+                        i = dirInstructions.Count - 1;
+                    }
+                    break;
+                case "WEST":
+                    if (arr[i - 1] == "EAST")
+                    {
+                        dirInstructions.RemoveAt(i);
+                        dirInstructions.RemoveAt(i - 1);
+                        i = dirInstructions.Count - 1;
+                    }
+                    break;
+            }
+        }
+
+        return dirInstructions.ToArray();
+    }
+}
---
+++ directions-reduction/tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+        string[] b = new string[] { "WEST" };
+        Assert.AreEqual(b, DirReduction.dirReduc(a));
+    }
+    [Test]
+    public void Test2()
+    {
+        string[] a = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+        string[] b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+        Assert.AreEqual(b, DirReduction.dirReduc(a));
+    }
+}
